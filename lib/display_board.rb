800eaf7dbdbc936ad96c988c3377c12721a3543a
Aurelien Xavier  Payong D <xavier.payong@gmail.com>
2018-01-02 13:08:33 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,8 +2,9 @@
 # out the current state.
 
 board=["","","","","","","","",""]
-board[0] = O
-board[4] = X
+#board[0] = O
+#board[4] = X
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[3]} "
   puts "-----------"
