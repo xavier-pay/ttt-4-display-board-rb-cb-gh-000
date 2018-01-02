9e87eb4b2c2da401c9dabe84db9265914db8d513
Aurelien Xavier  Payong D <xavier.payong@gmail.com>
2018-01-02 13:09:15 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,8 +6,8 @@ board=["","","","","","","","",""]
 #board[4] = X
 
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[3]} "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts " #{board[4]} | #{board[5]} | #{board[6]} "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " #{board[7]} | #{board[8]} | #{board[9]} "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
